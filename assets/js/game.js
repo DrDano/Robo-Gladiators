f13baafd1943b6d1180eebe7b88256bda9fb04ea
DrDano <danielharned@gmail.com>
2021-11-12 13:10:22 -0800

    creating alert to display playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,9 @@
-var PlayerName = window.prompt("What is your robot's name?").toLocaleUpperCase()
+var playerName = window.prompt("What is your robot's name?")
+window.alert(playerName)
 
 function fight() {
     window.alert("The fight has begun!");
 }
 
-fight();
+// fight();
+
