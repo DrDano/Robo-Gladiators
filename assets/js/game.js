20d60cb2d8034be4ea900c03f55a356a590b2f71
DrDano <danielharned@gmail.com>
2021-11-12 17:15:15 -0800

    adding comments for function features to add, and logged message

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,6 +8,16 @@ var enemyAttack = 12;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of 'playerAttack' from the value of 'enemyhealth' and use that result to upate the value in the 'enemyHealth' variable
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + "attacked" + enemyName + ". " + enemyName + "now has " + enemyHealth + " health remaining.");
+
+    // Subtract the value of 'enemyAttack' from the value of playerHealth and use that result to update the value in the 'playerHealth' variable.
+
+    // Log a resulting message to the console so we know that it worked.
+
 };
 
 fight()
