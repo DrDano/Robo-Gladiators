33e82388be945b4f7769b522f1ffeeffa7d0b76d
DrDano <danielharned@gmail.com>
2021-11-12 17:43:11 -0800

    adding conditional statement to determine if enemy has died

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,6 +15,13 @@ var fight = function() {
     // Log a resulting message to the console so we know that it worked.
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
+    // Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    };
+
     // Subtract the value of 'enemyAttack' from the value of playerHealth and use that result to update the value in the 'playerHealth' variable.
     playerHealth -= enemyAttack
 
